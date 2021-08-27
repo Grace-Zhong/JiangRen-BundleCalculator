62945a610131cc0d00ebe6b0c0045d8488cc4786
Grace-Zhong <250467347@qq.com>
2021-08-27 23:39:39 +0930

    Optimized a bit, added README.md

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,5 @@
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.Map;
 
@@ -22,24 +21,42 @@ public class Calculator {
         video.put(9,new BigDecimal(1530));
         data.put("VID", video);
 
-        calTotal(10, 15, 19, data);
+        calTotal(10, 15, 13, data);
     }
 
     public static void calTotal(int inputImgNum, int inputFlacNum, int inputVidNum, HashMap<String, TreeMap<Integer, BigDecimal>> table) {
         System.out.println("------------------");
-        System.out.println("VID");
-        TreeMap<Integer, BigDecimal> subTable = table.get("VID");
-        TreeMap<String, BigDecimal> resImg = calSingle(inputVidNum, subTable);
+        System.out.println("IMG");
+        TreeMap<Integer, BigDecimal> subTableImg = table.get("IMG");
+        TreeMap<String, BigDecimal> resImg = calSingleType(inputImgNum, subTableImg);
         for (Map.Entry<String, BigDecimal> entry:resImg.entrySet()) {
             System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
         }
         System.out.println("------------------");
+        System.out.println("------------------");
+        System.out.println("FLAC");
+        TreeMap<Integer, BigDecimal> subTableFlac = table.get("FLAC");
+        TreeMap<String, BigDecimal> resFlac = calSingleType(inputFlacNum, subTableFlac);
+        for (Map.Entry<String, BigDecimal> entry:resFlac.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+        System.out.println("------------------");
+        System.out.println("------------------");
+        System.out.println("VID");
+        TreeMap<Integer, BigDecimal> subTableVid = table.get("VID");
+        TreeMap<String, BigDecimal> resVid = calSingleType(inputVidNum, subTableVid);
+        for (Map.Entry<String, BigDecimal> entry:resVid.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+        System.out.println("------------------");
+
     }
 
-    public static TreeMap<String, BigDecimal> calSingle(int inputNum, TreeMap<Integer, BigDecimal> subTable) {
+    // return the cheapest solution of a single type social media
+    public static TreeMap<String, BigDecimal> calSingleType(int inputNum, TreeMap<Integer, BigDecimal> subTable) {
         TreeMap<String, BigDecimal> res = new TreeMap<>();
 
-        // find the smallest value 的 key 是多少
+        // if inputNum is smaller than the cheapest bundle
         int cheapestBundle = smallestBundle(subTable);
         if (inputNum <= cheapestBundle) {
             res.put(String.valueOf(cheapestBundle), new BigDecimal(1));
@@ -47,7 +64,7 @@ public class Calculator {
         }
         else {
             // find threshold
-            int threshold = 0;      // threshold = sum of all keys
+            int threshold = 0;              // threshold = sum of all keys
             for (Integer key:subTable.keySet()) {
                 threshold = threshold + key;
             }
@@ -56,42 +73,47 @@ public class Calculator {
             int cutBundleBase = 0;         // record the redundant number of bundle
             int cut = 0;
             if (inputNum > threshold) {
-                // 需要 cut 的部分
-                cutBundleBase = smallestUnitBundle(subTable);       // 需要 cut 的 bundle key
-                cut = (int)Math.ceil((double)(inputNum - threshold)/cutBundleBase);    // 向上取整
+                cutBundleBase = smallestUnitBundle(subTable);       // the bundle name should be cut
+                cut = (int)Math.ceil((double)(inputNum - threshold)/cutBundleBase);    // the number of bundle should be cut
                 inputNum = inputNum - cutBundleBase * cut;
             }
 
             // calculate the cheapest bundle
-            res = calBasic(inputNum, subTable);
+            res = calBasicCase(inputNum, subTable);
 
             // add the redundant number of bundle
-            res.put(String.valueOf(cutBundleBase), res.get(String.valueOf(cutBundleBase)).add(new BigDecimal(cut)));
-            BigDecimal tmpBigDec = (new BigDecimal(cut)).multiply(subTable.get(cutBundleBase));
-            res.put("Total", res.get("Total").add(tmpBigDec));
+            if (cut != 0) {
+                res.put(String.valueOf(cutBundleBase), res.get(String.valueOf(cutBundleBase)).add(new BigDecimal(cut)));
+                BigDecimal tmpBigDec = (new BigDecimal(cut)).multiply(subTable.get(cutBundleBase));
+                res.put("Total", res.get("Total").add(tmpBigDec));
+            }
+
         }
 
         return res;
     }
 
-    public static TreeMap<String, BigDecimal> calBasic(int remain, TreeMap<Integer, BigDecimal> currentTable) {
+    public static TreeMap<String, BigDecimal> calBasicCase(int remain, TreeMap<Integer, BigDecimal> currentTable) {
         TreeMap<String, BigDecimal> res = new TreeMap<>();
+
+        // initialization: the number of all key (beside the last key) is 0
+        BigDecimal minCost = (new BigDecimal(Math.ceil((double)remain/currentTable.lastKey())))
+                .multiply(currentTable.lastEntry().getValue());
+        for (Map.Entry<Integer, BigDecimal> entry:currentTable.entrySet()) {
+            res.put(String.valueOf(entry.getKey()), new BigDecimal(0));
+        }
+        res.put(String.valueOf(currentTable.lastKey()), new BigDecimal(Math.ceil((double)remain/currentTable.lastKey())));
+        res.put("Total", minCost);
+
+        // find the cheapest solution
         // base case
         if (currentTable.size() == 2) {
-            // first key 为 0
-            BigDecimal minCost = new BigDecimal(Math.ceil((double)remain/currentTable.lastKey()))
-                    .multiply(currentTable.lastEntry().getValue());
-            res.put(currentTable.firstKey().toString(), new BigDecimal(0));
-            res.put(currentTable.lastKey().toString(), new BigDecimal(Math.ceil((double)remain/currentTable.lastKey())));
-            res.put("Total", minCost);
-
             int firstKey = currentTable.firstKey();
             int maxNumOfFirstKey = (int)Math.ceil((double)remain/firstKey);
             for (int i = 1; i <= maxNumOfFirstKey; i++) {
                 BigDecimal numOfLastKey = new BigDecimal(Math.ceil((double)(remain-currentTable.firstKey() * i) / currentTable.lastKey()));
                 BigDecimal currentSum = (new BigDecimal(i)).multiply(currentTable.firstEntry().getValue())
                         .add(numOfLastKey.multiply(currentTable.lastEntry().getValue()));
-                //如果当前钱数小于 min，min 为新钱数
                 if (currentSum.compareTo(minCost) == -1) {
                     minCost = currentSum;
                     res.clear();
@@ -102,34 +124,19 @@ public class Calculator {
             }
         }
         else {
-            // 全要最后一个 key
-            BigDecimal minCost = (new BigDecimal(Math.ceil((double)remain/currentTable.lastKey())))
-                    .multiply(currentTable.lastEntry().getValue());
-            for (Map.Entry<Integer, BigDecimal> entry:currentTable.entrySet()) {
-                res.put(String.valueOf(entry.getKey()), new BigDecimal(0));
-            }
-            res.put(String.valueOf(currentTable.lastKey()), new BigDecimal(Math.ceil((double)remain/currentTable.lastKey())));
-
-
-            res.put("Total", minCost);
-
             int firstKey = currentTable.firstKey();
             int maxNumOfFirstKey = (int)Math.ceil((double)remain/firstKey);
             for (int i = 0; i <= maxNumOfFirstKey; i++) {
                 TreeMap<Integer, BigDecimal> dynamicTable = new TreeMap<>();
                 dynamicTable.putAll(currentTable);
-                // 记录第一个要花多少钱， i * firstKey
                 BigDecimal costOfFirstKey = currentTable.firstEntry().getValue().multiply(new BigDecimal(i));
-                // record removed key & value
-                String recordedKey = String.valueOf(currentTable.firstKey());
-
                 dynamicTable.remove(dynamicTable.firstKey());
-                TreeMap<String, BigDecimal> returnMap = calBasic(remain - currentTable.firstKey() * i, dynamicTable);
+                TreeMap<String, BigDecimal> returnMap = calBasicCase(remain - currentTable.firstKey() * i, dynamicTable);
                 BigDecimal currentTotalCost = returnMap.get(returnMap.lastKey()).add(costOfFirstKey);
                 if (currentTotalCost.compareTo(minCost) == -1) {
                     minCost = currentTotalCost;
                     res.putAll(returnMap);
-                    res.put(recordedKey, new BigDecimal(i));
+                    res.put(String.valueOf(currentTable.firstKey()), new BigDecimal(i));
                     res.put("Total", currentTotalCost);
                 }
             }
@@ -137,7 +144,7 @@ public class Calculator {
         return res;
     }
 
-    // 返回总价最便宜的 bundle 数
+    // Return the bundle number with the lowest bundle price
     public static int smallestBundle(TreeMap<Integer, BigDecimal> subTable) {
         int res = 0;
         BigDecimal minValue = subTable.firstEntry().getValue();
@@ -150,7 +157,7 @@ public class Calculator {
         return res;
     }
 
-    // 返回单价最低的 bundle 数
+    // Return the bundle number with the lowest unit price
     public static int smallestUnitBundle(TreeMap<Integer, BigDecimal> subTable) {
         int res = 0;
         BigDecimal minValue =subTable.firstEntry().getValue().divide(new BigDecimal(subTable.firstKey()));
